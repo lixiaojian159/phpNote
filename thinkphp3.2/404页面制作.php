f9417750239ecd4cddbd06ba9c642d5516fb07f7
lijian <852688838@qq.com>
2018-08-24 00:33:15 +0800

    tp3.2的404页面

--- thinkphp3.2/404页面制作.php
+++ thinkphp3.2/404页面制作.php
@@ -1,5 +1,13 @@
 <?php
 
 /**
+* 404页面
 *
+*  框架：thinkpgp3.2
+*
+*  一般都是写在 CommonController.class.php 控制器中  的error()方法
+*  然后别的控制器类 继承 CommonController.class.php
+*  别的控制器中的方法一点判断系统出错, 就调用CommonController.class.php中的 error    return $this->error('前端页面');
+*
+*  前端页面：自由发挥
 */
