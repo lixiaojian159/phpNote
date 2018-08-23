9d5e92dfa378060e996740d339b778dff4d56189
lijian <852688838@qq.com>
2018-08-24 00:40:39 +0800

    404

--- thinkphp3.2/404页面制作.php
+++ thinkphp3.2/404页面制作.php
@@ -6,8 +6,9 @@
 *  框架：thinkpgp3.2
 *
 *  一般都是写在 CommonController.class.php 控制器中  的error()方法
-*  然后别的控制器类 继承 CommonController.class.php
-*  别的控制器中的方法一点判断系统出错, 就调用CommonController.class.php中的 error    return $this->error('前端页面');
+*  （别的功能类  继承----> CommonController类  继承---> Controller类 ） (use Think\Controller;)
+*  然后别的控制器类 继承 CommonController这个类
+*  别的控制器中的方法一点判断系统出错, 就调用CommonController这个类中的 error    return $this->error('前端页面');
 *
 *  前端页面：自由发挥
 */
