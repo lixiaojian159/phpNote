3a5de4f06014c1ad928e9e20ace88b05e812642e
lijian <852688838@qq.com>
2018-09-17 22:54:57 +0800

    艺术家

---
+++ laravel/artisan命令.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ *  artisan 命令
+ *  php artisan list  所有的 artisan 命令
+ */
+
+/**
+ *   php artisan key : generate          生成APP key(.env配置文件里面的)
+ *   php artisan make: constroller name  生成控制器
+ *   php artisan make: model       name  生成模型
+ *   php artisan make: policy      name  生成授权策略
+ *   php artisan make: seeder      name  生成 Seeder 文件
+ *   php artisan migrate                 执行迁移
+ *   php artisan migrate: rollback       回滚迁移(上一次)
+ *   php artisan migrate: refresh        重置数据库
+ *   php artisan db: seed                填充数据库
+ *   php artisan tinker                  进入tinker环境
+ *   php artisan route:list              路由器列表
+ */
