2cef105c954b84ae731f28906676f9245d646700
lijian <852688838@qq.com>
2018-12-04 17:30:29 +0800

    oop

--- php/oop/oop.php
+++ php/oop/oop.php
@@ -140,5 +140,88 @@ EOT;
  /**
   * 类文件的自动加载
   * __autoload()  当我们引入一个不存在的类时, 自动调用他导入该类文件
-  * 自定义导入函数, 用sql_autoload_register()
+  * 自定义导入函数, 用sql_autoload_register('自定义函数名')
   */
+
+
+  /**
+   * __construct(){}
+   * 构造函数  :  用来实例化类, 创建对象的(时候自动执行)
+   * __destrcut(){}
+   * 析构函数  :  用来销毁属性的, 对象销毁时自动调用
+   */
+
+
+  /**
+   * 对象的封装
+   * 访问修饰符 private
+   * public function  __get($name){
+   *    return $this->$name;
+   * }
+   *
+   * public function __set($name,$value){
+   *     $this->$name = $value;
+   * }
+   */
+
+  /**
+   *   __isset(){} 检测是否存在某个属性
+   *   __unset(){} 在类外部销毁某个私有属性时, 自动调用他
+   */
+
+   /**
+    * 类的继承和多态的实现办法
+    * 访问修饰符 protected
+    * 这里申明是受保护的, 这样只能被子类继承, 子类继承过去仍然是protected
+    * 声明子类(扩展类),  继承关键字extends ,  php是单继承语言
+    * 创建子类的功能： 扩展父类的功能, 实现代码复用
+    * 在子类重写父类方法时, 其访问权限不能低于原来的, 原来的时protected , 现在应该是public
+    */
+
+   /**
+    * 类中的静态属性的创建和访问
+    * 访问修饰符  指示类成员在哪里可以被访问 public/protected/private
+    * 成员状态符  指示如何访问该成员  静态self/parent/static  非静态$this
+    * 静态属性只能在静态方法中访问 , 静态属性不能用伪变量$this
+    *  self::静态变量名    (本类)
+    *  parent::静态变量名  (子类继承父类,子类调用父类中的静态属性)
+    *  在外部使用对象, 也可以访问静态方法
+    *  在外部不能访问静态属性
+    */
+
+   /**
+    * 类的静态绑定和延迟绑定
+    * 重点(理解) 编译时机不同
+    */
+
+   /**
+    * 对象都是引用赋值
+    * 克隆,相当于值赋值  ()
+    */
+
+class Demo{
+
+ 	 public $name = 'Peter';
+}
+
+$obj1 = new Demo();
+
+$obj2 = $obj1;
+
+if($obj1 === $obj2){
+ 	 echo '相等<br/>';
+}else{
+ 	 echo '不同<br/>';
+}
+var_dump($obj1);
+var_dump($obj2);
+
+$obj3 = clone $obj1;
+
+if($obj3 === $obj1){
+	 echo '相同<br/>';
+}else{
+	 echo '不同<br/>';
+}
+
+var_dump($obj3);
